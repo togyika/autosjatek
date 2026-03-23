860de0819c44fd2fec6467e73c5a7e320518dddc
Rendszergazda <rendszergazda@blathy.info>
2026-03-23 12:06:09 +0100

    Add project files.

---
+++ autosjatek/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace autosjatek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
